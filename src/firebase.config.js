56c9e4c47112a8ab46676614b23ecfe794702cf2
ismail360deg <ismailbhuiyan2000@gmail.com>
2023-03-01 21:35:38 +0600

    Done project

--- src/firebase.config.js
+++ src/firebase.config.js
@@ -5,12 +5,12 @@ import {getFirestore} from 'firebase/firestore';
 import {getStorage} from 'firebase/storage';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAWirNUlMWY0HLEoIX7CNrs5DoFL3n9BTE",
-  authDomain: "e-shopping-983ef.firebaseapp.com",
-  projectId: "e-shopping-983ef",
-  storageBucket: "e-shopping-983ef.appspot.com",
-  messagingSenderId: "772487428539",
-  appId: "1:772487428539:web:5b7c5ca2f7fd178d25ec9e"
+  apiKey: process.env.REACT_APP_apiKey,
+  authDomain: process.env.REACT_APP_authDomain,
+  projectId: process.env.REACT_APP_projectId,
+  storageBucket: process.env.REACT_APP_storageBucket,
+  messagingSenderId: process.env.REACT_APP_messagingSenderId,
+  appId: process.env.REACT_APP_appId,
 };
 
 // Initialize Firebase
@@ -20,3 +20,5 @@ export const db = getFirestore(app);
 export const storage = getStorage(app);
 
 export default app;
+
+
